9fd10a5fa4707571a225dbc883b11694431fd18c
Justin New <JNew@tql.com>
2026-08-06 11:26:13 -0400

    Fix sign-in page background.

--- src/account/SignInPage.tsx
+++ src/account/SignInPage.tsx
@@ -4,6 +4,8 @@ import toast from "react-hot-toast";
 import { userAPI } from "../users/UserAPI";
 import { useUserContext } from "../App";
 import { IUser } from "../users/IUser";
+import "../App";
+import "../index.css";
 
 interface IAccount {
   username: string;
@@ -28,8 +30,8 @@ function SignInPage() {
   const signin: SubmitHandler<IAccount> = async (account) => {
     try {
       const { password: _, ...safeUser } = await userAPI.findByAccount(account.username, account.password);
-      persistUser(safeUser as IUser); // localStorage
-      setUser(safeUser as IUser); // context
+      persistUser(safeUser as IUser);
+      setUser(safeUser as IUser);
       navigate("/requests");
     } catch (error: any) {
       toast.error("Unsuccessful sign in. Please try again.");
@@ -37,7 +39,7 @@ function SignInPage() {
   };
 
   return (
-    <main className="d-flex flex-column gap-4 justify-content-center align-items-center min-vh-100">
+    <main className="signin d-flex flex-column gap-4 justify-content-center align-items-center min-vh-100">
       <svg width={80} height={62} viewBox="0 0 50 39" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z" fill="#007AFF" />
         <path d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z" fill="#312ECB" />
